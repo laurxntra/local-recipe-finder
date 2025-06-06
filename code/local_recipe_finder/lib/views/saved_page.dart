a7a46c7a8cebf6cedb16b728d6ea6c0621880f97
Jaime White <jaimele@cs.washington.edu>
2025-06-05 23:25:36 -0700

    save page semantics

--- code/local_recipe_finder/lib/views/saved_page.dart
+++ code/local_recipe_finder/lib/views/saved_page.dart
@@ -10,10 +10,10 @@ class SavedPage extends StatelessWidget {
 
   @override
   /// Builds the widget for the saved recipes page
-  /// 
+  ///
   /// Parameters:
   /// - context: Used to access the provider
-  /// 
+  ///
   /// Returns:
   /// - The UI of lists of saved recipe cards/a message indicating no saved recipes yet
   Widget build(BuildContext context) {
@@ -22,90 +22,109 @@ class SavedPage extends StatelessWidget {
     final savedRecipes = provider.likedRecipes;
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Saved Recipes'),
-        centerTitle: true,
-      ),
-      body: savedRecipes.isEmpty
-      // Displays a message if no recipes have been saved yet
-          ? const Center(
-              child: Text(
-                "No saved recipes yet. Start swiping!",
-                style: TextStyle(fontSize: 16),
-              ),
-            )
-            // If there are saved recipes, showing the user in a scrollable list
-          : ListView.builder(
-              itemCount: savedRecipes.length,
-              itemBuilder: (context, index) {
-                final recipe = savedRecipes[index];
-                return GestureDetector(
-                  onTap: () async {
-                    final updatedRecipe = await Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (_) => RecipeDetailsPage(recipe: recipe),
+      appBar: AppBar(title: const Text('Saved Recipes'), centerTitle: true),
+      body:
+          savedRecipes.isEmpty
+              // Displays a message if no recipes have been saved yet
+              ? const Center(
+                child: Text(
+                  "No saved recipes yet. Start swiping!",
+                  style: TextStyle(fontSize: 16),
+                ),
+              )
+              // If there are saved recipes, showing the user in a scrollable list
+              : ListView.builder(
+                itemCount: savedRecipes.length,
+                itemBuilder: (context, index) {
+                  final recipe = savedRecipes[index];
+                  return GestureDetector(
+                    onTap: () async {
+                      final updatedRecipe = await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => RecipeDetailsPage(recipe: recipe),
+                        ),
+                      );
+                      if (updatedRecipe != null) {
+                        final provider = Provider.of<LocalRecipeFinderProvider>(
+                          context,
+                          listen: false,
+                        );
+                        provider.updatedRecipe(updatedRecipe);
+                      }
+                    },
+                    child: Semantics(
+                      label: 'Recipe Card for ${recipe.name}',
+                      button: true,
+                      child: Card(
+                        margin: const EdgeInsets.all(12),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              // Recipe name
+                              Semantics(
+                                header: true,
+                                label: 'Recipe Name for: ${recipe.name}',
+                                child: Text(
+                                  recipe.name,
+                                  style: const TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 8),
+                              // Recipe image
+                              Semantics(
+                                label: 'Image for ${recipe.name}',
+                                image: true,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: Image.network(
+                                    recipe.imageUrl,
+                                    height: 180,
+                                    width: double.infinity,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 10),
+                              // Recipe Ingredients
+                              const Text(
+                                'Ingredients:',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              ...recipe.ingredients.map(
+                                (item) => Text("• $item"),
+                              ),
+                              const SizedBox(height: 10),
+                              // Recipe instructions
+                              Semantics(
+                                label: 'Instructions for ${recipe.name}',
+                                header: true,
+                                child: Text(
+                                  'Instructions:',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              Text(
+                                recipe.instructions.isNotEmpty
+                                    ? recipe.instructions.first
+                                    : "No instructions available.",
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                    );
-                    if (updatedRecipe != null) {
-                      final provider = Provider.of<LocalRecipeFinderProvider>(context, listen: false);
-                      provider.updatedRecipe(updatedRecipe);
-                    }
-                  },
-                child: Card(
-                  margin: const EdgeInsets.all(12),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        // Recipe name
-                        Text(
-                          recipe.name,
-                          style: const TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const SizedBox(height: 8),
-                        // Recipe image
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Image.network(
-                            recipe.imageUrl,
-                            height: 180,
-                            width: double.infinity,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        // Recipe Ingredients
-                        const Text(
-                          'Ingredients:',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        ...recipe.ingredients.map((item) => Text("• $item")),
-                        const SizedBox(height: 10),
-                        // Recipe instructions
-                        const Text(
-                          'Instructions:',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          recipe.instructions.isNotEmpty
-                              ? recipe.instructions.first
-                              : "No instructions available.",
-                        ),
-                      ],
                     ),
-                  ),
-                )
-              );
-            },
-          ),
+                  );
+                },
+              ),
     );
   }
 }
